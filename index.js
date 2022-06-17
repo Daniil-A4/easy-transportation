d00773db1a3fae88cca2f2e613525d0f6e6553b7
Daniil-A4 <dani.beshlebnov@gmail.com>
2022-06-17 17:13:43 +0300

    convert myid

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const nodeMailer = require('nodemailer')
 const TelegramBot = require('node-telegram-bot-api')
 try {require('./config')} catch  {}
 const token = process.env.TOKEN
-const myId = process.env.MYID
+const myId = +process.env.MYID
 const bot = new TelegramBot(token, { polling: true })
 const port = process.env.PORT || 5000
 
